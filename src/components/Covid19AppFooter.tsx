2321cee65dc33a74f0e0820d52285c089991dd29
Harry Chan <harry.chan@mensa.org.hk>
2020-06-19 19:01:46 +0800

    removed the grid gutter from the footer

--- src/components/Covid19AppFooter.tsx
+++ src/components/Covid19AppFooter.tsx
@@ -3,7 +3,7 @@ import './Covid19AppFooter.scss'
 import packageJson from '../../package.json'
 
 const Covid19AppFooter = (): JSX.Element => {
-    return <footer className="covid19-app-footer p-grid">
+    return <footer className="covid19-app-footer p-grid p-nogutter">
         <div className="p-col-8">Powered by React and PrimeReact</div>
         <div className="p-col-4 version">version: {packageJson.version}</div>
     </footer>
