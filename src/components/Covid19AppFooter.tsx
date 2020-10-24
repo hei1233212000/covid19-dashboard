a227aea58a782ec2dd6a81ee1922d0225254fe40
Harry Chan <harry.chan@mensa.org.hk>
2020-10-24 16:59:09 +0800

    fixed build warning in App.int.test.tsx

--- src/components/Covid19AppFooter.tsx
+++ src/components/Covid19AppFooter.tsx
@@ -4,7 +4,7 @@ import packageJson from '../../package.json'
 
 const Covid19AppFooter = (): JSX.Element => {
     return <footer className="covid19-app-footer p-grid p-nogutter">
-        <div className="p-col-8">Powered by <a href="https://reactjs.org/" target="_blank">React</a> and <a href="https://www.primefaces.org/primereact/" target="_blank">PrimeReact</a></div>
+        <div className="p-col-8">Powered by <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://www.primefaces.org/primereact/" target="_blank" rel="noopener noreferrer">PrimeReact</a></div>
         <div className="p-col-4 version">version: {packageJson.version}</div>
     </footer>
 }
